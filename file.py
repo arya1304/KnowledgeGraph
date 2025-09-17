6c7143110e6aa7dcc27a55112fea30b98857fd98
Arya Thachil <a.thachil@student.unsw.edu.au>
2025-09-17 14:20:07 +1000

    added query for coach

--- file.py
+++ file.py
@@ -1,4 +1,5 @@
-from rdflib import Graph, Literal, RDF, URIRef, Namespace
+from rdflib import Graph, Namespace
+import re
 
 g = Graph()
 
@@ -9,14 +10,40 @@ g.add((BAD.LinDan, BAD.playsFor, BAD.China))
 g.add((BAD.PVSindhu, BAD.playsFor, BAD.India))
 g.add((BAD.ViktorAxelsen, BAD.playsFor, BAD.Denmark))
 
-playerName = input()
+g.add((BAD.LeeChongWei, BAD.coachedBy, BAD.MisbunSidek))
+g.add((BAD.LinDan, BAD.coachedBy, BAD.XiaXuanze))
+g.add((BAD.PVSindhu, BAD.coachedBy, BAD.KimJi))
+g.add((BAD.ViktorAxelsen, BAD.coachedBy, BAD.PeterGade))
+
+playerName = input("Enter player name: ")
+
+# remove spaces and join the name
+name = playerName.split()
+name = "".join(name)
 
 qres = g.query(f"""
     SELECT ?country
     WHERE {{
-        <http://example.org/badminton/{playerName}> <http://example.org/badminton/playsFor> ?country .
+        <http://example.org/badminton/{name}> <http://example.org/badminton/playsFor> ?country .
     }}
 """)
 
+coachres = g.query(f"""
+    SELECT ?coach
+    WHERE {{
+        <http://example.org/badminton/{name}> <http://example.org/badminton/coachedBy> ?coach .
+    }}
+""")
+
+
 for row in qres:
     print(f"{playerName} plays for:", row[0].split("/")[-1])
+
+for row in coachres:
+    coachName = row[0].split("/")[-1]
+    match = r'(?=[A-Z])'
+    n = re.split(match, coachName)
+    for i in n:
+        if i == "":
+            n.remove(i)
+    print(f"{playerName} is coached by:",     " ".join(n))
